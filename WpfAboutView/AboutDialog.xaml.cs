9eaa6fe30aa3dff6758437160a7fd91f5602f47c
Daniel Chalmers <daniel.chalmers@outlook.com>
2018-01-11 15:19:47 -0500

    Add AboutDialog

---
+++ WpfAboutView/AboutDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace WpfAboutView
+{
+    /// <summary>
+    /// Interaction logic for AboutDialog.xaml
+    /// </summary>
+    public partial class AboutDialog : Window
+    {
+        public static readonly DependencyProperty AboutViewProperty =
+            DependencyProperty.Register(
+                nameof(AboutView),
+                typeof(AboutView),
+                typeof(AboutDialog));
+
+        public AboutDialog()
+        {
+            InitializeComponent();
+
+            AboutView = new AboutView();
+        }
+
+        public AboutView AboutView
+        {
+            get => (AboutView)GetValue(AboutViewProperty);
+            set => SetValue(AboutViewProperty, value);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
