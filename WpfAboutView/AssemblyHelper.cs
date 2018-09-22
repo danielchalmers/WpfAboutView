46018f13a117471076ec77a4859f20975b98f411
Daniel Chalmers <daniel.chalmers@outlook.com>
2018-09-22 14:40:06 -0500

    Refactor AssemblyHelper and handle missing members

--- WpfAboutView/AssemblyHelper.cs
+++ WpfAboutView/AssemblyHelper.cs
@@ -6,16 +6,52 @@ namespace WpfAboutView
 {
     internal static class AssemblyHelper
     {
-        internal static string GetCompany(this Assembly _assembly) => _assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company;
+        /// <summary>
+        /// Returns the assembly's Company attribute, or <c>null</c> if one isn't supplied.
+        /// </summary>
+        internal static string GetCompany(this Assembly assembly) =>
+           assembly.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company;
 
-        internal static string GetCopyright(this Assembly _assembly) => _assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright;
+        /// <summary>
+        /// Returns the assembly's Copyright attribute, or <c>null</c> if one isn't supplied.
+        /// </summary>
+        internal static string GetCopyright(this Assembly assembly) =>
+           assembly.GetCustomAttribute<AssemblyCopyrightAttribute>()?.Copyright;
 
-        internal static string GetDirectory(this Assembly _assembly) => Path.GetDirectoryName(_assembly.GetLocation());
+        /// <summary>
+        /// Returns the assembly's Description attribute, or <c>null</c> if one isn't supplied.
+        /// </summary>
+        internal static string GetDescription(this Assembly assembly) =>
+           assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
 
-        internal static string GetLocation(this Assembly _assembly) => _assembly.Location;
+        /// <summary>
+        /// Returns info about the assembly's directory.
+        /// </summary>
+        internal static DirectoryInfo GetDirectoryInfo(this Assembly assembly) =>
+           assembly.GetFileInfo().Directory;
 
-        internal static string GetTitle(this Assembly _assembly) => _assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        /// <summary>
+        /// Returns info about the assembly's file.
+        /// </summary>
+        internal static FileInfo GetFileInfo(this Assembly assembly) =>
+           new FileInfo(assembly.Location);
 
-        internal static Version GetVersion(this Assembly _assembly) => _assembly.GetName().Version;
+        /// <summary>
+        /// Returns the assembly's Title attribute, or <c>null</c> if one isn't supplied.
+        /// </summary>
+        internal static string GetProduct(this Assembly assembly) =>
+           assembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product;
+
+        /// <summary>
+        /// Returns the assembly's Title attribute, or <c>null</c> if one isn't supplied.
+        /// </summary>
+        internal static string GetTitle(this Assembly assembly) =>
+           assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title;
+
+        /// <summary>
+        /// Returns the assembly's Version attribute, or <c>null</c> if one isn't supplied.
+        /// </summary>
+        internal static Version GetVersion(this Assembly assembly) =>
+           assembly.GetName()?.Version;
     }
 }
